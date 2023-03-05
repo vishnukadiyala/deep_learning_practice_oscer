14e8bc6ec343d105a3d2863aaca008d0e1a06194
Vishnu PK AML <Vishnu.P.Kadiyala-1@ou.edu>
2023-03-05 17:49:40 -0600

    hw3 deep net

--- hw3/hw3_original.sh
+++ hw3/hw3_original.sh
@@ -3,15 +3,15 @@
 #SBATCH --partition=normal
 #SBATCH --cpus-per-task=10
 # memory in MB
-#SBATCH --mem=15000
-#SBATCH --output=results/hw3_%04a_stdout.txt
-#SBATCH --error=results/hw3_%04a_stderr.txt
-#SBATCH --time=08:00:00
-#SBATCH --job-name=hw3
-#SBATCH --mail-user=YOUR OWN EMAIL ADDRESS
+#SBATCH --mem=30000
+#SBATCH --output=results/run1/hw3_%04a_stdout.txt
+#SBATCH --error=results/run1/hw3_%04a_stderr.txt
+#SBATCH --time=48:00:00
+#SBATCH --job-name=HW3_Run1 
+#SBATCH --mail-user=vishnupk@ou.edu
 #SBATCH --mail-type=ALL
-#SBATCH --chdir=YOUR OWN DIRECTORY
-##SBATCH --array=1
+#SBATCH --chdir=/home/cs504305/deep_learning_practice/homework/hw3
+#SBATCH --array=0-4
 #
 #################################################
 # Do not change this line unless you have your own python/tensorflow/keras set up
@@ -20,5 +20,4 @@
 conda activate tf
 
 # 2023
-python hw3_base.py @oscer.txt @exp.txt @net_shallow.txt --exp_index $SLURM_ARRAY_TASK_ID --cpus_per_task $SLURM_CPUS_PER_TASK
-
+python hw3_base.py @oscer.txt @exp_deep.txt @net_deep.txt --exp_index $SLURM_ARRAY_TASK_ID --cpus_per_task $SLURM_CPUS_PER_TASK --results_path results/run1
